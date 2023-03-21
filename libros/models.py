55b9a46f548e50c4e7c256104796a445b6bb152c
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-03-21 14:50:15 -0600

    add  UNIT-TEST #3

--- libros/models.py
+++ libros/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Libro(models.Model):
@@ -6,7 +7,7 @@ class Libro(models.Model):
     autor = models.TextField (default='',blank=False)
     genero = models.TextField (default='',blank=False)
     editorial = models.TextField (default='',blank=False)
-    anio = models.IntegerField(default = 'yyyy',blank=False)
+    anio = models.IntegerField(default = 0,blank=False)
     num_pages = models.IntegerField(default=0,blank=False)
     costo = models.DecimalField(default=0, max_digits=5, decimal_places=2,blank=False)
     categoria = models.TextField (default=' ', blank=False)
--- libros/tests.py
+++ libros/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from graphene_django.utils.testing import GraphQLTestCase
+from mixer.backend.django import mixer
+import graphene
+import json
+
+# Create your tests here.
+from libros.schema import schema
+from libros.models import Libro
+
+LIBROS_QUERY = '''
+ {libros { 
+    id
+    titulo
+    autor 
+    genero 
+    editorial 
+    anio 
+    numPages 
+    costo 
+    categoria 
+    edicion 
+    idioma}}
+'''
+
+class LibroTestCase(GraphQLTestCase):
+    GRAPHQL_SCHEMA = schema
+    def setUp(self):
+        self.libro1 = mixer.blend(Libro)
+        self.libro2 = mixer.blend(Libro)
+
+    def test_libros_query(self):
+        response = self.query(
+            LIBROS_QUERY,
+        )
+
+
+        content = json.loads(response.content)
+        #print(content)
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+        print ("query libros results ")
+        print (content)
+        assert len(content['data']['libros']) == 2
