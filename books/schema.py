c163f11c06dff11b011769a81bbaf48096a8fd22
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-05-23 13:52:11 -0600

    Close Branch authentication

--- books/schema.py
+++ books/schema.py
@@ -1,12 +1,18 @@
 import graphene
 
 import libros.schema
+import users.schema
+import graphene
+import graphql_jwt
 
 
-class Query(libros.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, libros.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(libros.schema.Mutation, graphene.ObjectType):
-    pass
+class Mutation(users.schema.Mutation, libros.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
