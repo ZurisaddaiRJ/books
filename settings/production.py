a186013fe55606655bdeba1093083e3baaf2f36f
ZurisaddaiRJ <111381333+ZurisaddaiRJ@users.noreply.github.com>
2023-05-30 14:13:13 -0600

    Update production.py

--- settings/production.py
+++ settings/production.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': PROD_DATABASE,
         'USER': PROD_USER,
         'PASSWORD': PROD_PASSWORD,
-        'HOST': PROD_HOST,
+        'HOST': 'mahmud.db.elephantsql.com',
         'PORT': 5432,
         #'TEST': {
         #    'NAME': TEST_DATABASE,
